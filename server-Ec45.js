2b942ed872a61bd3dcff58873358152c2f73e6d0
chede_yugandhar <yogichede159@gmail.com>
2016-09-20 12:52:30 +0530

    Initial Commit

---
+++ server-Ec45.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
+});
